25f9b65407301004f2fed78025746657ecc249b7
Vian Maulana Ramadhan <vianmaulana540@gmail.com>
2023-03-01 12:53:43 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pzn', function () {
+    return "Hello Programmer Zaman Now";
+});
+
+Route::redirect('/youtube', '/pzn');
+
+Route::fallback(function () {
+    return "404 by PZN";
+});
