0c6a7c956d8d11b90b3c810d9fb2472aea4c44c9
Vian Maulana Ramadhan <vianmaulana540@gmail.com>
2023-03-05 08:47:06 +0700

    Route Parameter

--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -27,4 +27,45 @@ class RoutingTest extends TestCase
             ->assertSeeText("404 by PZN");
     }
    
+    public function testRouteParameter()
+    {
+        $this->get('/products/1')
+            ->assertSeeText('Product 1');
+
+        $this->get('/products/2')
+            ->assertSeeText('Product 2');
+
+        $this->get('/products/1/items/XXX')
+            ->assertSeeText("Product 1, Item XXX");
+
+        $this->get('/products/2/items/YYY')
+            ->assertSeeText("Product 2, Item YYY");
+    }
+
+    public function testRouteParameterRegex()
+    {
+        $this->get('/categories/100')
+            ->assertSeeText("Category 100");
+
+        $this->get('/categories/salah')
+            ->assertSeeText("404 by PZN");
+    }
+
+    public function testRouteParameterOptional()
+    {
+        $this->get('/users/khannedy')
+            ->assertSeeText("User khannedy");
+
+        $this->get('/users')
+            ->assertSeeText("User 404");
+    }
+
+    public function testRouteConflict()
+    {
+        $this->get('/conflict/budi')
+            ->assertSeeText("Conflict budi");
+
+        $this->get('/conflict/eko')
+            ->assertSeeText("Conflict Eko Kurniawan Khannedy");
+    }
 }
