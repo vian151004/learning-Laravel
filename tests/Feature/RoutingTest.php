9cbcd20631fb8fbc327eeba252e32c413df8536f
Vian Maulana Ramadhan <vianmaulana540@gmail.com>
2023-03-06 13:18:43 +0700

    Named Route

--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -68,4 +68,13 @@ class RoutingTest extends TestCase
         $this->get('/conflict/eko')
             ->assertSeeText("Conflict Eko Kurniawan Khannedy");
     }
+
+    public function testNamedRoute()
+    {
+        $this->get('/produk/12345')
+            ->assertSeeText("Link http://localhost/products/12345");
+
+        $this->get('/produk-redirect/12345')
+            ->assertRedirect('/products/12345');
+    }
 }
