2a3b2dffe292655f4da2646ef4932003f40a18ef
Vian Maulana Ramadhan <vianmaulana540@gmail.com>
2023-03-22 06:43:50 +0700

    Input Type

--- tests/Feature/InputControllerTest.php
+++ tests/Feature/InputControllerTest.php
@@ -56,4 +56,13 @@ class InputControllerTest extends TestCase
         ])->assertSeeText('Apple Macbook Pro')
             ->assertSeeText('Samsung Galaxy S10');
     }
+
+    public function testInputType()
+    {
+        $this->post('/input/type', [
+            'name' => 'Budi',
+            'married' => 'true',
+            'birth_date' => '1990-10-10',
+        ])->assertSeeText("Budi")->assertSeeText("true")->assertSeeText("1990-10-10");
+    }
 }
